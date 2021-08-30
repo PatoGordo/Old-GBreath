771d4b06b4550d1056f53407f0f2e0db9e884846
PatoGordo <icariusv@gmail.com>
2021-08-30 01:21:49 +0000

    pwa manifest ready

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,39 @@ export default defineConfig({
       },
     }),
     VitePWA({
-      manifest: {},
+      manifest: {
+        theme_color: "#2DFFC5",
+        background_color: "#FFFFFF",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "GBreath",
+        short_name: "GBreath",
+        description:
+          "GBreath is an open-source project to help you with your breathing practice.",
+        icons: [
+          {
+            src: "/images/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/images/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/images/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
       workbox: {},
     }),
   ],
